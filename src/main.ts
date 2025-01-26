d79bbae1acbabbfd644b189f66fb07d4b9ea25f5
siderlsoft <sahid.hm.21@gmail.com>
2025-01-26 14:36:55 -0600

    Add dependencies, basic structure and router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
+import './main.css'
 import App from './App.vue'
+import { createPinia } from 'pinia';
+import router from './router';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.mount('#app');
---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="p-5 text-center">
+      <h1 class="text-2xl font-bold">Home Page</h1>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    name: 'Home',
+  };
+  </script>
+  
+  <style scoped>
+  </style>
