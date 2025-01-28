d6cf98a1e72219502f3f7f91cdf60b2218d3a041
siderlsoft <sahid.hm.21@gmail.com>
2025-01-27 23:07:42 -0600

    add router

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './main.css'
 import App from './App.vue'
 import { createPinia } from 'pinia';
-import router from './router';
+import router from '../src/router';
 
 const app = createApp(App);
 app.use(createPinia());
